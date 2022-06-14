052edeeed3fa67bf7c1efbe3900eeb3f63c56505
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-14 12:43:25 +0530

    UC1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace EmployeeWageComputation
 {
-    class Program
+    public class UC1_EmployeeWage
     {
-        public static void Main(string[]args)
+        //Constants
+        public int Is_Full_Time = 1;
+    }
+    public class Program
+    {
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+            //Computation
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            UC1_EmployeeWage emp = new UC1_EmployeeWage();
+
+            if (empcheck == emp.Is_Full_Time)
+            {
+                Console.WriteLine("employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("employee is Absent");
+            }
         }
     }
 }
